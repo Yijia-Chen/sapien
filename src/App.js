deb15a3c1355367597525f7b107d75203dec3565
Yijia-Chen <yijiachenmain@gmail.com>
2021-06-23 13:51:36 +0800

    feat: update icon, title, and page layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container">
+          <div className="col-md-6">
+            <p>Wow</p>
+          </div>
+          <div className="col-md-6">
+            <p>Doge</p>
+          </div> 
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
